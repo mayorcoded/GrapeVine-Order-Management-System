491137d26c2e31213dae2c95d99cd22749ebf6f2
Mayowa <mtudonu85@gmail.com>
2018-03-24 19:59:42 +0100

    Add functionalities for order resource

--- src/main/java/com/learn/starter/resource/OrderResource.java
+++ src/main/java/com/learn/starter/resource/OrderResource.java
@@ -1,4 +1,37 @@
 package com.learn.starter.resource;
 
-public class OrderResource {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.learn.starter.domain.Order;
+import org.springframework.hateoas.ResourceSupport;
+
+public class OrderResource extends ResourceSupport {
+
+    private final long id;
+    private final String description;
+    private final long costInCents;
+    private final boolean isComplete;
+
+    public OrderResource(Order order){
+        id = order.getId();
+        description = order.getDescription();
+        costInCents = order.getCostInCents();
+        isComplete = order.isComplete();
+    }
+
+    @JsonProperty("id")
+    public Long getResourceId(){
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public long getCostInCents(){
+        return costInCents;
+    }
+
+    public boolean isComplete(){
+        return isComplete;
+    }
 }
