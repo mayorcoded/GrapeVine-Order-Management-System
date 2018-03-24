cb7deb7d2b659f85d347b9f298dd504732abb1fa
Mayowa <mtudonu85@gmail.com>
2018-03-24 19:58:53 +0100

    Add functionalities for order resource assmebler

---
+++ src/main/java/com/learn/starter/resource/OrderResourceAssembler.java
@@ -0,0 +1,29 @@
+package com.learn.starter.resource;
+
+import com.learn.starter.domain.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.Link;
+
+
+public class OrderResourceAssembler extends ResourceAssembler<Order, OrderResource>{
+
+    @Autowired
+    protected EntityLinks entityLinks;
+
+    private static final String UPDATE_REL = "updates";
+    private static final String DELETE_REL = "delete";
+
+    @Override
+    public OrderResource toResource(Order order) {
+        OrderResource resource = new OrderResource(order);
+
+        final Link selfLink = entityLinks.linkToSingleResource(order);
+        
+        resource.add(selfLink.withSelfRel());
+        resource.add(selfLink.withRel(UPDATE_REL));
+        resource.add(selfLink.withRel(DELETE_REL));
+
+        return resource;
+    }
+}
