71266116aaaf7f849c5adcd08ec85a6b4d13e38c
Mayowa <mtudonu85@gmail.com>
2018-03-24 20:11:21 +0100

    Add component annotation

--- src/main/java/com/learn/starter/repository/OrderRepository.java
+++ src/main/java/com/learn/starter/repository/OrderRepository.java
@@ -1,4 +1,14 @@
 package com.learn.starter.repository;
 
-public class OrderRepository {
+import com.learn.starter.domain.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderRepository extends InMemoryRepository<Order> {
+    @Override
+    protected void updateIfExists(Order original, Order updated) {
+        original.setDescription(updated.getDescription());
+        original.setCostInCents(updated.getCostInCents());
+        original.setComplete(updated.isComplete());
+    }
 }
