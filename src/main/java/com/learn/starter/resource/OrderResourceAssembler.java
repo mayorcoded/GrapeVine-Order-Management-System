f5dbc4c30d998d0c9fb4e11bd0d78f267be34997
Mayowa <mtudonu85@gmail.com>
2018-03-24 20:10:27 +0100

    Remove autowire from hateoas library

--- src/main/java/com/learn/starter/resource/OrderResourceAssembler.java
+++ src/main/java/com/learn/starter/resource/OrderResourceAssembler.java
@@ -4,12 +4,12 @@ import com.learn.starter.domain.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Link;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class OrderResourceAssembler extends ResourceAssembler<Order, OrderResource>{
 
-    @Autowired
-    protected EntityLinks entityLinks;
+    private EntityLinks entityLinks;
 
     private static final String UPDATE_REL = "updates";
     private static final String DELETE_REL = "delete";
@@ -19,7 +19,7 @@ public class OrderResourceAssembler extends ResourceAssembler<Order, OrderResour
         OrderResource resource = new OrderResource(order);
 
         final Link selfLink = entityLinks.linkToSingleResource(order);
-        
+
         resource.add(selfLink.withSelfRel());
         resource.add(selfLink.withRel(UPDATE_REL));
         resource.add(selfLink.withRel(DELETE_REL));
