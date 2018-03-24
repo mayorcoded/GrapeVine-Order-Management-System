15f630b0733192e34dd4f75f4fb560a482d390dc
Mayowa <mtudonu85@gmail.com>
2018-03-24 21:51:13 +0100

    Add hypermedia annotations

--- src/main/java/com/learn/starter/StarterApplication.java
+++ src/main/java/com/learn/starter/StarterApplication.java
@@ -2,7 +2,11 @@ package com.learn.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableEntityLinks;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
+@EnableEntityLinks
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @SpringBootApplication
 public class StarterApplication {
 
